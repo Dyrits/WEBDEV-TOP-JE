3ea2d36acac33969eec3a44496e6b5cb56786563
Dyrits <dyrits@outlook.fr>
2020-05-03 17:07:21 +0200

    sumAll solution has been added, using recursion.

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,6 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+  if (start === end) { return end; }
+  return start + sumAll(start + 1, end);
+};
 
-}
-
-module.exports = sumAll
+module.exports = sumAll;
