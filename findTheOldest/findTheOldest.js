50559ec8bef30ca1a696f90eb7582834ec578741
Dyrits <dyrits@outlook.fr>
2020-05-06 16:11:19 +0200

    findTheOldest solution has been added.

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-let findTheOldest = function() {
-
+let findTheOldest = function(array) {
+  return array.sort((objectOne, objectTwo) => {
+    if(!objectOne.yearOfDeath) {objectOne.yearOfDeath = (new Date).getFullYear(); }
+    if(!objectTwo.yearOfDeath) {objectTwo.yearOfDeath = (new Date).getFullYear(); }
+    return (objectTwo.yearOfDeath - objectTwo.yearOfBirth) - (objectOne.yearOfDeath - objectOne.yearOfBirth);
+  })[0]
 }
 
 module.exports = findTheOldest
