625b06b1eaf27d165d9d2f5d3c1ef89b84c00ea7
Dyrits <dyrits@outlook.fr>
2020-05-06 15:37:27 +0200

    Caesar solution has been added.

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,19 @@
-const caesar = function() {
-
+const caesar = function(string, shift) {
+  if (shift > 26) { shift %= 26; }
+  return string.split("").map(letter => {
+    if ((letter.charCodeAt(0) > 64 && letter.charCodeAt(0) < 91)) {
+      var codeChar = letter.charCodeAt(0) + shift
+      if(codeChar > 90) { codeChar -= 26; } 
+      else if(codeChar < 65) { codeChar += 26; }
+    }
+    if (letter.charCodeAt(0) > 96 && letter.charCodeAt(0) < 123) {
+      var codeChar = letter.charCodeAt(0) + shift
+      if(codeChar > 122) { codeChar -= 26; } 
+      else if(codeChar < 97) { codeChar += 26; }
+    }
+    if (codeChar) { return String.fromCharCode(codeChar); }
+    return letter;
+  }).join("");
 }
 
 module.exports = caesar
