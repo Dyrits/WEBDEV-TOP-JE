fd5a51e768633766a86ab78c9bd5fb13f675ef9d
Dyrits <dyrits@outlook.fr>
2020-05-06 15:52:06 +0200

    Fibonacci solution has been added.

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,6 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+  if (number < 0) { return "OOPS"; }
+  return number < 2 ? Number(number) : fibonacci(number - 1) + fibonacci(number - 2);
 }
 
 module.exports = fibonacci
