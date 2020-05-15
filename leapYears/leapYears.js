d08c4d07e550193593dbbb1cb0dd2616841c5d8c
Dyrits <dyrits@outlook.fr>
2020-05-15 15:15:04 +0200

    Minor update.

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,5 @@
 const leapYears = function(year) {
-  return year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0);
+  return !(year % 400) || (!(year % 4) && !!(year % 100));
 }
 
 module.exports = leapYears
--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,9 @@
 const sumAll = function (start, end) {
   if (start === end) { return end; }
+  if (start < 0 || end < 0 || typeof start != "number" || typeof end != "number") { 
+    return "Error";
+  }
+  if (end < start) { [start, end] = [end, start] }
   return start + sumAll(start + 1, end);
 };
 
