9d9dc0b378faacdb3c825eb2abbed10c6e36218e
Dyrits <dyrits@outlook.fr>
2020-05-16 13:53:04 +0200

    Solution updated

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,16 +1,8 @@
-const caesar = function(string, shift) {
-  if (shift > 26) { shift %= 26; }
+const caesar = function (string, shift) {
+  shift < 0 ? shift = shift % 26 + 26 : shift %= 26;
   return string.split("").map(letter => {
-    if ((letter.charCodeAt(0) > 64 && letter.charCodeAt(0) < 91)) {
-      var codeChar = letter.charCodeAt(0) + shift
-      if(codeChar > 90) { codeChar -= 26; } 
-      else if(codeChar < 65) { codeChar += 26; }
-    }
-    if (letter.charCodeAt(0) > 96 && letter.charCodeAt(0) < 123) {
-      var codeChar = letter.charCodeAt(0) + shift
-      if(codeChar > 122) { codeChar -= 26; } 
-      else if(codeChar < 97) { codeChar += 26; }
-    }
+    if (/[a-z]/.test(letter)) { var codeChar = (letter.charCodeAt(0) + shift - 97) % 26 + 97 }
+    if (/[A-Z]/.test(letter)) { var codeChar = (letter.charCodeAt(0) + shift - 65) % 26 + 65 }
     if (codeChar) { return String.fromCharCode(codeChar); }
     return letter;
   }).join("");
